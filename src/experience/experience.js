fdaf81e8e1a42a0da97dbf98233ffbb233563f56
Felicia Chang <felicia.chang@yale.edu>
2019-06-13 11:33:43 -0700

    adding compile functionality

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -1,43 +1,49 @@
-import React, {Component} from 'react';
-import { Row, Col } from 'reactstrap';
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
 
 var divStyle = {
-    margin: "50px",
-    font: 'Helvetica'
-}
+  margin: "50px",
+  font: "Helvetica"
+};
 var bodyStyle = {
-    margin: "80px"
-}
+  margin: "80px"
+};
 
 class Experience extends Component {
   render() {
     return (
-      <div style = {divStyle}>
-        <header >
-            <h1>{"// Experience"} </h1>
+      <div style={divStyle}>
+        <header>
+          <h1>{"// Experience"} </h1>
         </header>
-        <br></br>
-        <div >
-            <br></br>
-            <h3> MeridianLink:  Management Intern </h3>
-            <p> Feb 2019 - Now</p>
-            <br></br>
-            <h3> Conservify Frontend:  Software Engineer Intern </h3>
-            <p> Dec 2018 - Now </p>
-            <br></br>
-            <h3> Weibo:  User Interface Design Intern </h3>
-            <p> June 2018 - Aug 2018 </p>
-            <br></br>
-            <h3> Tsai City Center for Innovation:  Student Ambassador </h3>
-            <p> Nov 2017 - Oct 2018 </p>
-            <br></br>
-            <p> For more details or to connect, find me on
-            <a href = "https://www.linkedin.com/in/felicia-chang-20666112a/"> Linkedin </a></p>
+        <br />
+        <div>
+          <br />
+          <h3> Thinkspaces: Cofounder</h3>
+          <p> Sept 2017 - Now</p>
+          <br />
+          <h3> Conservify Frontend: Software Engineer Intern </h3>
+          <p> Dec 2018 - Now </p>
+          <br />
+          <h3> Weibo: User Interface Design Intern </h3>
+          <p> June 2018 - Aug 2018 </p>
+          <br />
+          <h3> Tsai City Center for Innovation: Student Ambassador </h3>
+          <p> Nov 2017 - Oct 2018 </p>
+          <br />
+          <p>
+            {" "}
+            For more details or to connect, find me on
+            <a href="https://www.linkedin.com/in/felicia-chang-20666112a/">
+              {" "}
+              Linkedin{" "}
+            </a>
+          </p>
         </div>
-        <br></br>
-        <br></br>
+        <br />
+        <br />
 
-        <hr></hr>
+        <hr />
       </div>
     );
   }
--- src/home/home.js
+++ src/home/home.js
@@ -1,58 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
+import "./home.css";
 
 var divStyle = {
-    margin: "50px"
-}
+  marginTop: 50,
+  paddingLeft: "10%"
+};
 var bodyStyle = {
-    margin: "80px"
-}
+  margin: "80px"
+};
+
+const TogglePhoto = show => {
+  console.log(show);
+  if (show) {
+    return <img src="https://felswebsite.s3.amazonaws.com/IMG_2949.GIF" />;
+  } else {
+    return <div />;
+  }
+};
 
 class AboutMe extends Component {
+  state = {
+    compile: false
+  };
+
   render() {
+    console.log(this.state.compile);
     return (
-      <div style = {divStyle}>
-        <header >
-            <h1>{"// Felicia Chang "}</h1>
+      <div style={divStyle}>
+        <header>
+          <h1>{"// Felicia Chang "}</h1>
         </header>
-        <p style = {bodyStyle}>
-            {"int main (About me) {"}
-            <br></br>
-            &emsp; {"while ( Felicia is at Yale University ) {"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"she studies in Computer Science;"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"enjoys working on cool web projects;"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"is interested in product management;"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"and intersections between tech and art;"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"plays on the varsity golf team;"}
-            <br></br>
-            &emsp;&emsp;&emsp; {"eats a lot of bell peppers;"}
-            <br></br>
-            &emsp;{"}"}
-            <br></br>
-            <br></br>
-            &emsp;{"for ( Felicia = present, Felicia < old age, ++ Felicia) {"}
-            <br></br>
-            &emsp;&emsp;&emsp;{"you may contact her at:"}
-            <br></br>
-            &emsp;&emsp;&emsp;{"return("}
-            <a href = "mailto:felicia.chang@yale.edu"> felicia.chang@yale.edu </a>
-            {");"}
-            <br></br>
-            <br></br>
-            &emsp;{"if (you would like to check out some of her work) {"}
-            <br></br>
-            &emsp;&emsp;&emsp;{"scroll on down;"}
-            <br></br>
-            &emsp;{"}"}
-            <br></br>
-            {"}"}
-            <br></br>
-        </p>
-        <hr></hr>
+        <Row>
+          <Col>
+            <p style={bodyStyle}>
+              {"int main (About me) {"}
+              <br />
+              &emsp; {"while ( Felicia is at Yale University ) {"}
+              <br />
+              &emsp;&emsp;&emsp; {"she studies in Computer Science;"}
+              <br />
+              &emsp;&emsp;&emsp; {"enjoys working on cool web projects;"}
+              <br />
+              &emsp;&emsp;&emsp; {"is interested in product management;"}
+              <br />
+              &emsp;&emsp;&emsp; {"and intersections between tech and art;"}
+              <br />
+              &emsp;&emsp;&emsp; {"plays on the varsity golf team;"}
+              <br />
+              &emsp;&emsp;&emsp; {"eats a lot of bell peppers;"}
+              <br />
+              &emsp;{"}"}
+              <br />
+              <br />
+              &emsp;
+              {"for ( Felicia = present, Felicia < old age, ++ Felicia) {"}
+              <br />
+              &emsp;&emsp;&emsp;{"you may contact her at:"}
+              <br />
+              &emsp;&emsp;&emsp;{"return("}
+              <a href="mailto:felicia.chang@yale.edu">
+                {" "}
+                felicia.chang@yale.edu{" "}
+              </a>
+              {");"}
+              <br />
+              <br />
+              &emsp;{"if (you would like to check out some of her work) {"}
+              <br />
+              &emsp;&emsp;&emsp;{"scroll on down;"}
+              <br />
+              &emsp;{"}"}
+              <br />
+            </p>
+            <div
+              className="compile-button"
+              onClick={() => {
+                this.setState({ compile: !this.state.compile });
+              }}
+            >
+              Compile Me
+            </div>
+          </Col>
+          <Col>
+            {this.state.compile ? (
+              <div className="image-container">
+                <img src="https://felswebsite.s3.amazonaws.com/IMG_2949.GIF" />
+              </div>
+            ) : (
+              <div />
+            )}
+          </Col>
+        </Row>
+        <hr />
       </div>
     );
   }
